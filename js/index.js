62c3b6fa80c3e005407cbd593970431f7b19d281
Timothee Deve <timothee.deve@gmail.com>
2015-12-04 15:08:46 +0000

    Does a bit of cleanup

--- js/index.js
+++ js/index.js
@@ -1,66 +1,66 @@
-$(function () {
+$(document).ready(function() {
     var keyListener = new window.keypress.Listener();
 
-    var $el = $(".square");
-    var degree = 0;
 
+    // Function to build object triangle for player 1 or 2
     var triangleMaker = function(player) {
+        var self = this;
         this.el = $(".triangle"+player);
-        this.fullPlayer = "player"+player;
         this.degree = 0;
         var animateLeft;
         var animateRight;
 
-
-        this.rotate = function(cwOrCcw) {
+        // Rotate triangle 
+        // cwOrCcw: rotate clock wise or anti clock wise takes "cw" or "acw"
+        this.rotate = function(cwOrAcw) {
             
             var anim;
             
             anim = setInterval(function() {
-                if(player1.degree < 0) {
-                    player1.degree = 359;
+                if(self.degree < 0) {
+                    self.degree = 359;
                 }
-                else if (player1.degree > 360) {
-                    player1.degree = 1;
+                else if (self.degree > 360) {
+                    self.degree = 1;
                 }
 
-                $el.css({
-                    WebkitTransform: 'rotate(' + player1.degree + 'deg)'
+                self.el.css({
+                    WebkitTransform: 'rotate(' + self.degree + 'deg)'
                 });
 
-                if (cwOrCcw === "right") {
-                    player1.degree++;
+                if (cwOrAcw === "cw") {
+                    self.degree++;
                 }
-                else if (cwOrCcw === "left") {
-                    player1.degree--;
+                else if (cwOrAcw === "acw") {
+                    self.degree--;
                 }
                 
             }, 1);
 
-            if (cwOrCcw === "right") {
+            if (cwOrAcw === "cw") {
                 animateRight = anim;
             }
-            else if (cwOrCcw === "left") {
+            else if (cwOrAcw === "acw") {
                 animateLeft = anim;
             }
 
 
         };
 
-
+        // Build event listener for the movement keys.
         this.buildControl = function(){
 
             keyListener.register_combo({
                 "keys"              : "g",
                 "prevent_repeat"    : true,
-                "on_keydown"        : function(){player1.rotate("left");},
+                "on_keydown"        : function(){self.rotate("acw");},
                 "on_keyup"          : function(){clearInterval(animateLeft);}
             });
 
             keyListener.register_combo({
                 "keys"              : "h",
                 "prevent_repeat"    : true,
-                "on_keydown"        : function(){player1.rotate("right");},
+                "on_keydown"        : function(){self.rotate("cw");},
                 "on_keyup"          : function(){clearInterval(animateRight);}
             });
 
@@ -69,76 +69,9 @@ $(function () {
         
     };
 
+
+    // Initialise
     var player1 = new triangleMaker(1);
     player1.buildControl();
 
 });
-
-
-
-
-// function keeper() {
-//     var keyListener = new window.keypress.Listener();
-
-//     var $el = $(".square");
-//     var degree = 0;
-//     var animateRight;
-//     var animateLeft;
-
-
-
-//     var gKeyUpFunc = function() {
-//         animateLeft = setInterval(function() {
-//             if(degree < 0) {
-//                 degree = 359;
-//             }
-//             else if (degree > 360) {
-//                 degree = 1;
-//             }
-//             $el.css({
-//                 WebkitTransform: 'rotate(' + degree + 'deg)'
-//             });
-//             degree--;
-//         }, 1);
-//     };
-
-//     var gKeyDownFunc = function() {
-//         clearInterval(animateLeft);
-//     };
-
-//     keyListener.register_combo({
-//         "keys"              : "g",
-//         "prevent_repeat"    : true,
-//         "on_keydown"        : function(){gKeyUpFunc()},
-//         "on_keyup"          : function(){gKeyDownFunc()}
-//     });
-
-
-
-
-//     var hKeyUpFunc = function() {
-//         animateRight = setInterval(function() {
-//             if(degree < 0) {
-//                 degree = 359;
-//             }
-//             else if (degree > 360) {
-//                 degree = 1;
-//             }
-//             $el.css({
-//                 WebkitTransform: 'rotate(' + degree + 'deg)'
-//             });
-//             degree++;
-//         }, 1);
-//     };
-
-//     var hKeyDownFunc = function() {
-//         clearInterval(animateRight);
-//     };
-
-//     keyListener.register_combo({
-//         "keys"              : "h",
-//         "prevent_repeat"    : true,
-//         "on_keydown"        : function(){hKeyUpFunc()},
-//         "on_keyup"          : function(){hKeyDownFunc()}
-//     });
-// }
