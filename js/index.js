6fd91b3e7c04136e7e81453e5bd0ab347661c09b
Timothee Deve <timothee.deve@gmail.com>
2015-12-04 16:23:14 +0000

    Fixes indentation

--- js/index.js
+++ js/index.js
@@ -1,135 +1,140 @@
 $(document).ready(function() {
-    var keyListener = new window.keypress.Listener();
+	var keyListener = new window.keypress.Listener();
 
 
-    // Function to build object triangle for player 1 or 2
-    var triangleMaker = function(player) {
-        var self = this;
-        this.el = $(".triangle"+player);
-        this.degree = 0;
-        var animateLeft;
-        var animateRight;
+	// Function to build object triangle for player 1 or 2
+	var triangleMaker = function(player) {
+		var self = this;
+		this.el = $(".triangle"+player);
+		this.degree = 0;
+		var animateLeft;
+		var animateRight;
 
-        // Rotate triangle 
-        // cwOrCcw: rotate clock wise or anti clock wise takes "cw" or "acw"
-        this.rotate = function(cwOrAcw) {
-            
-            var anim;
-            
-            anim = setInterval(function() {
-                if(self.degree < 0) {
-                    self.degree = 359;
-                }
-                else if (self.degree > 360) {
-                    self.degree = 1;
-                }
+		// Rotate triangle 
+		// cwOrCcw: rotate clock wise or anti clock wise takes "cw" or "acw"
+		this.rotate = function(cwOrAcw) {
+			
+			var anim;
+			
+			anim = setInterval(function() {
+				if(self.degree < 0) {
+					self.degree = 359;
+				}
+				else if (self.degree > 360) {
+					self.degree = 1;
+				}
 
-                self.el.css({
-                    WebkitTransform: 'rotate(' + self.degree + 'deg)'
-                });
+				self.el.css({
+					WebkitTransform: 'rotate(' + self.degree + 'deg)'
+				});
 
-                if (cwOrAcw === "cw") {
-                    self.degree++;
-                }
-                else if (cwOrAcw === "acw") {
-                    self.degree--;
-                }
-                
-            }, 1);
+				if (cwOrAcw === "cw") {
+					self.degree++;
+				}
+				else if (cwOrAcw === "acw") {
+					self.degree--;
+				}
 
-            if (cwOrAcw === "cw") {
-                animateRight = anim;
-            }
-            else if (cwOrAcw === "acw") {
-                animateLeft = anim;
-            }
+			}, 1);
 
-
-        };
-
-        // Build event listener for the movement keys.
-        this.buildControl = function(){
-
-            keyListener.register_combo({
-                "keys"              : "g",
-                "prevent_repeat"    : true,
-                "on_keydown"        : function(){self.rotate("acw");},
-                "on_keyup"          : function(){clearInterval(animateLeft);}
-            });
-
-            keyListener.register_combo({
-                "keys"              : "h",
-                "prevent_repeat"    : true,
-                "on_keydown"        : function(){self.rotate("cw");},
-                "on_keyup"          : function(){clearInterval(animateRight);}
-            });
-
-        };
-
-        
-    };
+			if (cwOrAcw === "cw") {
+				animateRight = anim;
+			}
+			else if (cwOrAcw === "acw") {
+				animateLeft = anim;
+			}
 
 
-    var gameMaker = function() {
-        var self = this;
-        var wallArray = [0,0,0,0,0,0];
-        this.blockCounter = 0;
+		};
+
+		// Build event listener for the movement keys.
+		this.buildControl = function(){
+
+			keyListener.register_combo({
+				"keys"              : "g",
+				"prevent_repeat"    : true,
+				"on_keydown"        : function(){self.rotate("acw");},
+				"on_keyup"          : function(){clearInterval(animateLeft);}
+			});
+
+			keyListener.register_combo({
+				"keys"              : "h",
+				"prevent_repeat"    : true,
+				"on_keydown"        : function(){self.rotate("cw");},
+				"on_keyup"          : function(){clearInterval(animateRight);}
+			});
+
+		};
+
+		
+	};
 
 
-        this.makeBlock = function() {
-            self.blockCounter++;
-            $('.blockContainer').append('<div class="block" id="block'+self.blockCounter+'"></div>');
-        };
-
-        this.createWall = function(){
-          wallArray = [0,0,0,0,0,0];
-          var wallFullCounter = 0;
-
-            for (i=0; i<wallArray.length; i++) {
-
-                random = Math.random();
-                if (random < 0.66) {
-                    wallArray[i] = 1;
-                }
-                 
-            }
+	var gameMaker = function() {
+		var self = this;
+		var wallArray = [0,0,0,0,0,0];
+		this.blockCounter = 0;
 
 
-            for(i = 0; i < wallArray.length; i++) {
+		this.makeBlock = function() {
+			self.blockCounter++;
+			$('.blockContainer').append('<div class="block" id="block'+self.blockCounter+'"></div>');
+		};
 
-              if(wallArray[i] === 1) {
-                wallFullCounter++;
-              }
+		this.createWall = function(){
+			wallArray = [0,0,0,0,0,0];
+			var wallFullCounter = 0;
 
-              if(wallFullCounter >= 6) {
-                var wallPick = parseInt(Math.random() * 6);
-                wallArray[wallPick] = 0;
-              }
-            }
+				for (i=0; i<wallArray.length; i++) {
 
-        };
-
-
-    };
-
-
-
+					random = Math.random();
+					if (random < 0.66) {
+							wallArray[i] = 1;
+					}
+						 
+				}
 
 
+				for(i = 0; i < wallArray.length; i++) {
 
-    // Initialise
-    var game = new gameMaker();
-    var player1 = new triangleMaker(1);
-    player1.buildControl();
+					if(wallArray[i] === 1) {
+						wallFullCounter++;
+					}
+
+					if(wallFullCounter >= 6) {
+						var wallPick = parseInt(Math.random() * 6);
+						wallArray[wallPick] = 0;
+					}
+				}
+
+		};
+
+
+		this.spawnWall = function() {
+
+		};
+
+
+	};
 
 
 
 
 
-    keyListener.register_combo({
-        "keys"              : "t",
-        "prevent_repeat"    : true,
-        "on_keydown"        : function(){game.createWall();}
-    });
+
+	// Initialise
+	var game = new gameMaker();
+	var player1 = new triangleMaker(1);
+	player1.buildControl();
+
+
+
+
+
+	keyListener.register_combo({
+		"keys"              : "t",
+		"prevent_repeat"    : true,
+		"on_keydown"        : function(){game.createWall();}
+	});
 
 });
